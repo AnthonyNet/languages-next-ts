9b1303f1b376d307031c1c8f3c5f655a47a2592d
AnthonyZet <zejda.anthony@gmail.com>
2023-01-13 19:18:30 +0100

    comments added into irregular section

--- components/card/Card_Input.tsx
+++ components/card/Card_Input.tsx
@@ -2,94 +2,95 @@
 
 import { useState, useEffect } from "react";
 
-interface Props{
-
-  myData: string,
-
-
-   setTotalScore: React.Dispatch<React.SetStateAction<number>>,
-   setScore: React.Dispatch<React.SetStateAction<number>>,
-   setStars: React.Dispatch<React.SetStateAction<number>>,
-   placeholder: string,
-   setDefaultInput: React.Dispatch<React.SetStateAction<boolean>>,
-    defaultInput: boolean,
-    //totalScore: number|string[],
-    totalScore: number,
- }
- 
-
-const styles = {
-    li: 'p-2 font-bold',
-    li_green: 'p-2 bg-green-300 font-bold',
-    input: 'text-center border border-gray-300 text-bold',
+interface Props {
+  myData: string;
+  setTotalScore: React.Dispatch<React.SetStateAction<number>>;
+  setScore: React.Dispatch<React.SetStateAction<number>>;
+  setStars: React.Dispatch<React.SetStateAction<number>>;
+  placeholder: string;
+  defaultInput: boolean;
+  //totalScore: number|string[],
+  totalScore: number;
 }
 
-const Input = ({ myData, setTotalScore, setScore, setStars, placeholder, setDefaultInput, defaultInput, totalScore}: Props) => {
+const styles = {
+  li: "p-2 font-bold",
+  li_green: "p-2 bg-green-300 font-bold",
+  input: "text-center border border-gray-300 text-bold",
+};
+
+const Input = ({
+  myData,
+  setTotalScore,
+  setScore,
+  setStars,
+  placeholder,
+  defaultInput,
+  totalScore,
+}: Props) => {
   const [readOnly, setReadOnly] = useState(false);
   const [input, setInput] = useState("");
-  const [checkMistake, setCheckMistake] = useState('');
+  const [checkMistake, setCheckMistake] = useState("");
   const [checkStyles, setCheckStyles] = useState(false);
- 
 
   //Save Input value into INPUT variable
   const handleChange = (e: any) => {
     setInput(e.target.value);
   };
 
+  // Watch INPUT variable for changes
+  useEffect(() => {
+    runIT();
+    //eslint-disable-next-line
+  }, [input]);
+
+  const runIT = () => {
+    if (myData === input) {
+      // True === green border
+      setCheckStyles(true);
+      setReadOnly(true);
+
+      //Props
+      setTotalScore((count: number) => count + 1);
+      setScore((count: number) => count + 1);
+      setStars((count: number) => count + 1);
+      // localStorage.setItem("totalScore", JSON.stringify(totalScore + 1));
+      const number = totalScore + 1;
+
+      localStorage.setItem("totalScore", JSON.stringify(number));
+    }
 
 
-// Watch INPUT variable for changes
- useEffect(()=>{
-  runIT()
- //eslint-disable-next-line
- },[input])
-
-
- const runIT = () => {
-  if (myData === input) {
-    // True === green border
-    setCheckStyles(true);
-    setReadOnly(true);
-
-    //Props
-    setTotalScore((count:number) => count + 1);
-    setScore((count:number) => count + 1);
-    setStars((count:number) => count + 1);
-   // localStorage.setItem("totalScore", JSON.stringify(totalScore + 1));
-    const number = totalScore + 1;
-
-    localStorage.setItem("totalScore", JSON.stringify(number));
-  
-  }
-
-if(myData.startsWith(input)) {
-    //True === green text
-    setCheckMistake('text-green-500');
-} else {
-    setCheckMistake('text-red-500');
-}
-
- }
-
- //Check if defaultInput TRUE/FALSE 
-useEffect(()=>{
-  setCheckStyles(false);
-  setReadOnly(false);
-  setInput('');
-},[defaultInput])
-
+    if (myData.startsWith(input)) {
+      // True === GREEN TEXT
+      setCheckMistake("text-green-500");
+    } else {
+      setCheckMistake("text-red-500");
+    }
+  };
+/*------------------------------------------
+WATCH defaultInput for changes TRUE/FALSE
+setCheckStyles TRUE / FALSE
+setReadOnly TRUE / FALSE 
+setInput => CLEAR VALUE IN INPUT
+------------------------------------------*/
+ 
+  useEffect(() => {
+    setCheckStyles(false);
+    setReadOnly(false);
+    setInput("");
+  }, [defaultInput]);
 
   return (
-    <li className={checkStyles?styles.li_green:styles.li}>
+    <li className={checkStyles ? styles.li_green : styles.li}>
       <input
         type="text"
         value={input}
-        className={styles.input +'text-gray-500 ' + checkMistake }
+        className={styles.input + "text-gray-500 " + checkMistake}
         onChange={handleChange}
         readOnly={readOnly}
         placeholder={placeholder}
       />
-     
     </li>
   );
 };
--- sections/Irregular.tsx
+++ sections/Irregular.tsx
@@ -8,60 +8,71 @@ import CardHint from "../components/card/CardHint";
 
 import Score from "../components/card/Score";
 
-
-
 const styles = {
-  section: "section_Responsive flex justify-center items-center w-full h-[91vh] responsiveSection",
+  section:
+    "section_Responsive flex justify-center items-center w-full h-[91vh] responsiveSection",
   section_div:
     "max-w-sm card my-8 xl:my-0 border-4 border-double border-blue-700 rounded-lg shadow-xl shadow-slate-600 text-center",
   score_div: "py-3 px-6 border-b border-gray-300",
   btn_div: "border-t border-gray-300 text-gray-600",
+  h5: 'text-xl dark:bg-black font-medium mb-2 border-b border-gray-300',
+  ul: 'flex flex-col justify-around text-center mb-2',
+
 };
 
+function IrregularVerbs({ dataTS }: any) {
+  const [score, setScore] = useState(0);
+  const [stars, setStars] = useState(0);
+  const [hint, setHint] = useState(false);
+  const [rand, setRand] = useState(0);
+  const [defaultInput, setDefaultInput] = useState(false);
+  /*  const [totalScore, setTotalScore] = useState<string[]>([]); */
+  const [totalScore, setTotalScore] = useState<any>([]);
 
 
-function IrregularVerbs({ dataTS }:any) {
-    const [score, setScore] = useState(0);
-    const [stars, setStars] = useState(0);
-    const [hint, setHint] = useState(false);
-    const [rand, setRand] = useState(0);
-    const [defaultInput, setDefaultInput] = useState(false);
-   // const [totalScore, setTotalScore] = useState<string[]>([]);
-    const [totalScore, setTotalScore] = useState<any>([]);
+  /*------------------------------------------
+  SET LOCAL STRORAGE IF NOT EXISTS
+   ------------------------------------------*/
 
-    if (typeof window !== 'undefined') {
-      // do localStorage stuff here
-      const item = localStorage.getItem('totalScore')
-      if(item== null|| undefined){
-       // localStorage.setItem('totalScore',JSON.stringify('0'));
-        localStorage.setItem('totalScore', '0');
-      }
-    } 
+  if (typeof window !== "undefined") {
+    // do localStorage stuff here
+    const item = localStorage.getItem("totalScore");
+    if (item == null || undefined) {
+      // localStorage.setItem('totalScore',JSON.stringify('0'));
+      localStorage.setItem("totalScore", "0");
+    }
+  }
 
-    useEffect(() => {
-      setTotalScore(JSON.parse(localStorage.getItem('totalScore') || '' ));
-   //   setTotalScore(JSON.parse(localStorage.getItem('totalScore') || ''));
-    },[])
-   
+
+  /*------------------------------------------
+   SET TOTAL SCORE FROM LOCAL STORAGE ONE TIME
+  ------------------------------------------*/
+  useEffect(() => {
+    setTotalScore(JSON.parse(localStorage.getItem("totalScore") || ""));
+    //   setTotalScore(JSON.parse(localStorage.getItem('totalScore') || ''));
+  }, []);
+
+/*------------------------------------------
+3.
+AFTER CLICK ON <CardButton />
+GIVES RANDOM NUMBER
+RESET NUMBER OF STARS
+HIDE HINTS 
+------------------------------------------*/
 
   function randomWord() {
     setRand(Math.floor(Math.random() * dataTS.length));
     setStars(0);
     setHint(false);
-  
-   setDefaultInput(true);
+    setDefaultInput(true);
   }
 
- // create use effect to store totalScore in local storage
-
-
+ 
   return (
     <section className={styles.section}>
       <div className={styles.section_div}>
         <div className={styles.score_div}>
-          Celkové skóre: <span>{ 
-          totalScore
-          }</span>
+          Celkové skóre: <span>{totalScore}</span>
         </div>
         <div className={styles.score_div}>
           Momentální skóre: <span>{score > 0 ? score : 0}</span>
@@ -70,23 +81,19 @@ function IrregularVerbs({ dataTS }:any) {
         <Score score={stars} />
 
         <div className="px-6 inputs">
-          <h5 className={"text-xl dark:bg-black font-medium mb-2 border-b border-gray-300"}>
+          <h5 className={styles.h5}>
             {dataTS[rand].cz}
           </h5>
-        
-          <ul className="flex flex-col justify-around text-center mb-2">
-       
-          <Input
+
+          <ul className={styles.ul}>
+            <Input
               myData={dataTS[rand].base}
               setTotalScore={setTotalScore}
               setScore={setScore}
               setStars={setStars}
               placeholder={"Present Simple"}
               defaultInput={defaultInput}
-              setDefaultInput={setDefaultInput}
               totalScore={totalScore}
-             
-              
             />
             <Input
               myData={dataTS[rand].pastSimple}
@@ -95,23 +102,18 @@ function IrregularVerbs({ dataTS }:any) {
               setStars={setStars}
               placeholder="Past Simple"
               defaultInput={defaultInput}
-              setDefaultInput={setDefaultInput}
               totalScore={totalScore}
-             
-              
             />
             {dataTS[rand].pastSimple2 && (
               <Input
-
                 myData={dataTS[rand].pastSimple2}
                 setTotalScore={setTotalScore}
                 setScore={setScore}
                 setStars={setStars}
                 placeholder="Past Simple 2"
                 defaultInput={defaultInput}
-                setDefaultInput={setDefaultInput}
+
                 totalScore={totalScore}
-               
               />
             )}
             <Input
@@ -121,41 +123,32 @@ function IrregularVerbs({ dataTS }:any) {
               setStars={setStars}
               placeholder="Past Participle"
               defaultInput={defaultInput}
-              setDefaultInput={setDefaultInput}
               totalScore={totalScore}
-             
-              
             />
             {dataTS[rand].pastParticiple2 && (
               <Input
-
                 myData={dataTS[rand].pastParticiple2}
                 setTotalScore={setTotalScore}
                 setScore={setScore}
                 setStars={setStars}
                 placeholder="Past Participle 2"
                 defaultInput={defaultInput}
-                setDefaultInput={setDefaultInput}
+
                 totalScore={totalScore}
-               
-              
               />
             )}
           </ul>
-           {/*the function gives new random word & set up styles to default */}
-        <div className={styles.btn_div}>
-          <CardButton onClick={randomWord} text="next word" />
-        </div>
-  
+          {/* ---   3   ---- */ }
+          <div className={styles.btn_div}>
+            <CardButton onClick={randomWord} text="next word" />
+          </div>
         </div>
 
-       
-
         <div className={styles.btn_div}>
-          {/* button toggle hint !hint */}
+          {/* TOGGLE HINT !HINT */}
           <CardButton onClick={() => setHint((hint) => !hint)} text="hint" />
 
-          {/* if hint is true show answers */}
+          {/* IF HINT == TRUE SHOW ANSWERS */}
           {hint ? (
             <CardHint
               base={dataTS[rand].base}
