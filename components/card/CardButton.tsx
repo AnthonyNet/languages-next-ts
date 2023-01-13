daf76392090b8fd156d61f972b0678364cf5ef22
AnthonyZet <zejda.anthony@gmail.com>
2023-01-13 19:21:40 +0100

    comments added into irregular section

--- components/card/CardButton.tsx
+++ components/card/CardButton.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
-interface Button{
+interface Button {
   onClick: () => void;
   text: string;
 }
 
-const CardButton = ({onClick, text}:Button ) => {
+const CardButton = ({ onClick, text }: Button) => {
   return (
-   
-         <button
-            onClick={onClick}
-            type="submit"
-            className="input_Btn px-6 py-2.5 mt-2 mb-2 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            {text}
-          </button>
-    
-  )
-}
+    <button
+      onClick={onClick}
+      type="submit"
+      className="input_Btn px-6 py-2.5 mt-2 mb-2 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+    >
+      {text}
+    </button>
+  );
+};
 
-export default CardButton
+export default CardButton;
--- components/card/CardHint.tsx
+++ components/card/CardHint.tsx
@@ -1,25 +1,27 @@
-
 interface CardHint {
   base: string;
   pastSimple: string;
   pastSimple2?: string;
   pastParticiple: string;
   pastParticiple2?: string;
-
 }
 
-const CardHint = ({base, pastSimple, pastSimple2, pastParticiple, pastParticiple2}:CardHint) => {
+const CardHint = ({
+  base,
+  pastSimple,
+  pastSimple2,
+  pastParticiple,
+  pastParticiple2,
+}: CardHint) => {
   return (
     <ul className="flex justify-between flex-wrap">
-    <li>{base} - </li>
-    <li>{pastSimple} - </li>
-    {pastSimple2&&<li>{pastSimple2} - </li>}
-    <li>{pastParticiple}</li>
-    {pastParticiple2 ? (
-      <li> -{pastParticiple2}</li>
-    ) : null}
-  </ul>
-  )
-}
+      <li>{base} - </li>
+      <li>{pastSimple} - </li>
+      {pastSimple2 && <li>{pastSimple2} - </li>}
+      <li>{pastParticiple}</li>
+      {pastParticiple2 ? <li> -{pastParticiple2}</li> : null}
+    </ul>
+  );
+};
 
-export default CardHint
+export default CardHint;
