9cf36695a8b3e78e55f037904a0ac8661f2398ad
AnthonyZet <zejda.anthony@gmail.com>
2023-03-16 15:26:05 +0100

    framerM

--- components/oxford-goethe/Card.tsx
+++ components/oxford-goethe/Card.tsx
@@ -15,9 +15,12 @@ import {Card_Vocabs} from "../../interface/";
 
 const styles = {
   section : 'flex justify-center items-center w-full h-[90vh] p-2 sm:p-0 responsiveSection',
-  section_div: 'w-[600px]  border-4 border-double border-blue-300 rounded-lg shadow-xl shadow-slate-600',
+  section_div: 'w-[600px]  border-4 border-double border-blue-300 rounded-lg shadow-xl shadow-slate-600 preserve-3d group my-rotate-y-180 duration-1000',
   ul: 'card_Ul flex flex-col justify-around w-full text-center p-8 font-semibold italic',
   li: 'p-8 border-b-2 border-slate-300',
+
+  cardBack: "absolute top-0 my-rotate-y-180 backface-hidden overflow-hidden bg-white w-full h-full",
+cardBack__div: "text-center flex flex-col items-center text-gray-800"
 }
 
 const Card = ({ dataTS }:{ dataTS:Card_Vocabs[] }) => {
@@ -83,24 +86,19 @@ const Card = ({ dataTS }:{ dataTS:Card_Vocabs[] }) => {
     setHidden(true);
   };
 
-  return (
-    <section className={styles.section}>
-    <div className={styles.section_div}>
-      <ul className={styles.ul}>
-      {/* true = Foreign language to Czech false = Czech to Foeign language */}
-        {switchLanguage? <CardTrue />: <CardFalse />}
-
-        {hidden ? (
-          <li
-            className={"text-cyan-500 " + styles.li}
-            onClick={() => setHidden(!hidden)}
-          >
-            show answer
-          </li>
-        ) : null
-        }
-
-        <li className="btn_Li flex justify-around text-center mt-8">
+  return ( 
+  <motion.section
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 1 }}
+    className={styles.section + " relative flex items-center justify-center border-2 border-red-300 cursor-pointer group perspective"}
+  >
+    <motion.div
+      className={styles.section_div }
+      animate={{ rotateY: 180 }}>
+     
+     <li className="btn_Li flex justify-around text-center mt-8">
           <CardButton onClick={randomWord} text={"Next word"} />
 
           <CardButton
@@ -108,11 +106,19 @@ const Card = ({ dataTS }:{ dataTS:Card_Vocabs[] }) => {
             text={switchLanguage ? "CZ ➜ ENG" : "ENG ➜ CZ"}
           />
         </li>
-      </ul>
-    </div>
-  </section>
+  
+      <motion.div className="styles.cardback">
+      <div className={styles.cardBack__div}>
+            <h1 className="text-4xl">Back</h1>
+      </div>
+      </motion.div>
 
+      
+   
+ 
+   </motion.div>
 
+  </motion.section>
   );
 };
 
