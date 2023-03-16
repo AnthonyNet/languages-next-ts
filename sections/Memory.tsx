b47361bf878833cee9620cbae482dd8dd0051326
AnthonyZet <zejda.anthony@gmail.com>
2023-03-16 09:06:08 +0100

    nothing

--- sections/Memory.tsx
+++ sections/Memory.tsx
@@ -15,21 +15,22 @@ function Memory() {
 
  const [store, setStore] = useState<string[]>([])
 
-  const items = [
-     {letter: 'A', select: '1'}, 
-      {letter: 'B', select: '2'},
-      {letter: 'C', select: '3'},
-      {letter: 'D', select: '4'},
-      {letter: 'E', select: '5'},
-      {letter: 'F', select: '6'},
-      {letter: 'G', select: '7'},
-      {letter: 'H', select: '8'},
+
+  let items = [
+     {word: 'Auto', translate: "Car", select: '0', answered: false}, 
+      {word: 'Brick', translate: "Cihla", select: '1', answered: false},
+      {word: 'Card', translate: "Karta", select: '2', answered: false},
+      {word: 'Dope', translate: "Hustý", select: '3', answered: false},
+      {word: 'Ex', translate: "Bývalý", select: '4', answered: false},
+      {word: 'Fold', translate: "skládat", select: '5', answered: false},
+      {word: 'Goal', translate: "Gól", select: '6', answered: false},
+      {word: 'Hope', translate: "Naděje", select: '7', answered: false},
   ]
 /* ---------------------------- */
 /*        Setup random Cards          */
 /* ---------------------------- */
   function resetCards(){
-    const shuffled = [...items, ...items].sort(() => Math.floor(Math.random() * items.length));
+    const shuffled = [...items].sort(() => Math.floor(Math.random() * items.length));
    // const shuffled = [...items, ...items].sort(() => 0.5 - Math.random());
     setCards(shuffled)
     //console.log(shuffled);
@@ -41,42 +42,52 @@ function Memory() {
 
 /* ---------------------------- */
 
+function handleClick(item){
+
+setStore([...store, item.select])
+
+if(store[0] === item.select){
+ 
+  items[item.select].answered = true
+  console.log(items);
+  
+  return setStore([])
+}
+}
 
 /* ---------------------------- */
 /*       Compare Cards          */
 /* ---------------------------- */
 
 
-  useEffect(() => {
-    console.log(store);
-    
-    if(store.length === 2){
-      setTimeout(() => {
-        setStore([])
-      }, 1000)
-    }
 
-    if(store[0] === store[1] && store.length === 2){
-      
-     return alert('You found a match!')
-    }
-  }, [store])
 
   return (
     <section className="w-screen h-[90vh] flex flex-col items-center justify-center">
       <h3>Memory</h3>
       <article className="w-[80%] h-[90%] border-4 border-red-400 grid grid-cols-4 grid-rows-4 gap-4 grid-flow-row p-2">
         
-        {cards.map((item:Card, index:number) => {
+        {items.map((item, index) => {
           return (
-            <MemoryCard
-              key={index}
-              letter={item.letter}
-              store={store}
-              setStore={setStore}
-             
-            />
-          );
+            <div key={index}
+             className={"border-4 flex justify-center items-center " + (items[index].answered?' bg-green-400':null)}
+             onClick={() => handleClick(item)}
+             >
+              {item.word}
+            </div>
+          )
+        })
+        }
+
+      {cards.map((item, index) => {
+          return (
+            <div key={index}
+             className={`border-4 flex justify-center items-center ${items[index].answered?' bg-green-400':null}`}
+             onClick={() => handleClick(item)}
+             >
+              {item.translate}
+            </div>
+          )
         })
         }
        
