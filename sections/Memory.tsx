e9d7fea139dbe89d4fa342ad08dfcf6975d408d1
AnthonyZet <zejda.anthony@gmail.com>
2023-03-19 19:00:20 +0100

    data change menu is working

--- sections/Memory.tsx
+++ sections/Memory.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import MemoryCard from "../components/memoryCard/MemoryCard";
 
@@ -6,97 +6,109 @@ import { useState, useEffect } from "react";
 import { IrregularVerbs, VerbenData } from "../myData";
 
 interface Item {
-
   cz?: string;
   eng?: string;
   select: number;
   check: boolean;
   click: boolean;
-  }
- 
+}
 
 function Memory() {
- 
+  const [props, setProps] = useState<any>(IrregularVerbs);
 
-  const [score, setScore] = useState<number>(0)
-  const [cards, setCards] = useState<any>([])
-  const [store, setStore] = useState<number[]>([])
-  const [prev,setPrev] = useState<number>(-1)
+  const [score, setScore] = useState<number>(0);
+  const [cards, setCards] = useState<any>([]);
+  const [store, setStore] = useState<number[]>([]);
+  const [prev, setPrev] = useState<number>(-1);
 
-  const createData =  (english:any) => {
-   const RAW = [...english].sort(() => Math.floor(Math.random() * english.length)).slice(0, 8)
+  const createData = (english: any) => {
+    const RAW = [...english]
+      .sort(() => Math.floor(Math.random() * english.length))
+      .slice(0, 8);
 
-    const randomEnglish = RAW.map((item,index)=>({select: index, eng: item.base, check:false, click: false}))
+    const randomEnglish = RAW.map((item, index) => ({
+      select: index,
+      eng: item.base,
+      check: false,
+      click: false,
+    }));
 
-    const randomCzech = RAW.map((item,index)=>({select: index, cz: item.cz, check: false, click:false}))
-   
-    setCards([...randomEnglish, ...randomCzech].sort(() => Math.floor(Math.random() * randomEnglish.length)))
-  
-  }
+    const randomCzech = RAW.map((item, index) => ({
+      select: index,
+      cz: item.cz,
+      check: false,
+      click: false,
+    }));
 
+    setCards(
+      [...randomEnglish, ...randomCzech].sort(() =>
+        Math.floor(Math.random() * randomEnglish.length)
+      )
+    );
+  };
 
-
-/* ---------------------------- */
-/*        Setup random Cards    */
-/* ---------------------------- */
-
+  /* ---------------------------- */
+  /*        Setup random Cards    */
+  /* ---------------------------- */
 
   useEffect(() => {
-    createData(IrregularVerbs)
-   
-  }, [])
+    createData(props);
+  }, []);
 
-useEffect(() => {
- // console.log(store);
-  
-}, [store])
+  /* ---------------------------- */
+  /*        Check DATA/WORDS change - MENU    */
+  /* ---------------------------- */
 
-/* ---------------------------- */
-/*       Compare Cards          */
-/* ---------------------------- */
-function check(current:number){
-  if(cards[prev].select === cards[current].select){
-    
-    setScore(score + 1)
-    setStore([...store, cards[prev].select])
-    setCards(cards.map((item:Item, index:number) => {
-      if(index === prev || index === current){
-        return {...item, check: true, click: true}
-      }else {
-cards.map((card:Item) => card.click = false)
-setCards([...cards])
-        return item
-      }
-    }))
+  useEffect(() => {
+    createData(props);
+  }, [props]);
+
+  /* ---------------------------- */
+  /*       Compare Cards          */
+  /* ---------------------------- */
+  function check(current: number) {
+    if (cards[prev].select === cards[current].select) {
+      setScore(score + 1);
+      setStore([...store, cards[prev].select]);
+      setCards(
+        cards.map((item: Item, index: number) => {
+          if (index === prev || index === current) {
+            return { ...item, check: true, click: true };
+          } else {
+            cards.map((card: Item) => (card.click = false));
+            setCards([...cards]);
+            return item;
+          }
+        })
+      );
+    }
+    setPrev(-1);
   }
-  setPrev(-1)
-}
 
-function handleClick(id:number, select:number){
-cards[id].click = true
-setCards([...cards])
-if(prev === -1){
-  setPrev(id)
-  return
-}else {
-  cards.map((card:Item) => card.click = false)
-setCards([...cards])
- check(id)
-}
-}
+  function handleClick(id: number, select: number) {
+    cards[id].click = true;
+    setCards([...cards]);
+    if (prev === -1) {
+      setPrev(id);
+      return;
+    } else {
+      cards.map((card: Item) => (card.click = false));
+      setCards([...cards]);
+      check(id);
+    }
+  }
+
   return (
     <section className="w-screen h-[90vh] flex flex-col items-center justify-center">
       <div>
-        <button onClick={()=>setCards(IrregularVerbs)}>B1</button>
-        <button onClick={() => setCards(VerbenData)}>C1</button>
+        <button onClick={() => setProps(IrregularVerbs)}>B1</button>
+        <button onClick={() => setProps(VerbenData)}>C1</button>
       </div>
       <h3>Memory</h3>
-     <h2>{score}</h2>
-     
+      <h2>{score}</h2>
+
       <article className="w-[80%] h-[80%] border-4 border-red-400 grid grid-cols-4 grid-rows-4 gap-4 grid-flow-row p-2">
-     
-      {cards.map((item:Item, index:number) => {
-       
+        {cards.map((item: Item, index: number) => {
           return (
             <MemoryCard
               key={index}
@@ -105,9 +117,7 @@ setCards([...cards])
               handleClick={handleClick}
             />
           );
-        })
-        }
-       
+        })}
       </article>
     </section>
   );
