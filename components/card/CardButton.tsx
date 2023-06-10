b9e5d563e861004d4652b9608b9523317694700d
AnthonyNet <zejda.antonin@gmail.com>
2023-06-10 17:26:55 +0200

    refactor(oxford-goethe): added new design for buttons and icons
    flags instead text, new colors for buttons

--- components/card/CardButton.tsx
+++ components/card/CardButton.tsx
@@ -7,14 +7,13 @@ interface Button {
 
 const CardButton = ({ onClick, text }: Button) => {
   return (
-    <button
-      onClick={onClick}
-      type="submit"
-      className="input_Btn px-6 py-2.5 mt-2 mb-2 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-    >
-      {text}
-    </button>
-  );
+		<button
+			onClick={onClick}
+			type="submit"
+			className="input_Btn px-6 py-2.5 mt-2 mb-2 bg-green-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition  ease-in-out duration-500">
+			{text}
+		</button>
+	);
 };
 
 export default CardButton;
--- components/oxford-goethe/Card.tsx
+++ components/oxford-goethe/Card.tsx
@@ -1,11 +1,14 @@
 "use client";
+import { useState } from "react";
+import { Card_Vocabs } from "../../interface/";
+import Image from "next/image";
 
 import { motion } from "framer-motion";
-import { useState } from "react";
-import CardButton from "../card/CardButton";
+import Typewriter from "typewriter-effect";
 
-import { Card_Vocabs } from "../../interface/";
-
+//FLAGS images
+import czFlag from "../../myData/images/cz.png";
+import enFlag from "../../myData/images/eng.png";
 {
   /*
 
@@ -15,20 +18,22 @@ import { Card_Vocabs } from "../../interface/";
 }
 
 const styles = {
-  section:
-    "relative flex justify-center items-center p-2 sm:p-0 responsiveSection",
-  section__div:
-    "w-full sm:w-[600px] h-[250px] sm:h-[270px] md:h-[320px] border-double border-4 border-blue-300  rounded-lg shadow-xl shadow-slate-600 preserve-3d group my-rotate-y-180 duration-1000 flex flex-col justify-around",
-    article: "w-full flex flex-col justify-center sm:grow",
-  section__div__btnCover: "flex justify-around text-center p-2 sm:p-4 border-t-2 border-blue-400",
-  h3: "py-2 md:py-4  text-center",
-
-  cardBack:
-    "absolute top-0 my-rotate-y-180 backface-hidden overflow-hidden w-full h-full mainCardBack",
- 	 cardBack__div:
-    "h-full text-center flex flex-col items-center text-gray-600 ",
-    cardBack__answersCover: "w-full h-full flex flex-col justify-center md:p-4",
-  	cardBack__btnCover: "w-full flex justify-around p-2 md:p-4  border-t-2 border-blue-400",
+	section:
+		"relative flex justify-center items-center p-2 sm:p-0 responsiveSection",
+	section__div:
+		"w-full sm:w-[600px] h-[250px] sm:h-[270px] md:h-[320px] border-double border-4 border-blue-800  rounded-lg shadow-lg shadow-blue-800 preserve-3d group my-rotate-y-180 duration-1000 flex flex-col justify-around ",
+	article: "w-full flex flex-col justify-center sm:grow ",
+	section__div__btnCover:
+		"flex justify-around text-center border-t-2 border-blue-400",
+	h3: "py-2 md:py-4  text-center border-b border-blue-600 w-auto mx-auto",
+	button_green:
+		"cardBtn bg-green-600 hover:bg-green-800 focus:bg-green-800 active:bg-green-800",
+	button_red: "cardBtn bg-red-600 hover:bg-red-800 focus:bg-red-800 active:bg-red-800",
+	cardBack:
+		"absolute top-0 my-rotate-y-180 backface-hidden overflow-hidden w-full h-full mainCardBack",
+	cardBack__div: "h-full text-center flex flex-col items-center text-gray-600 ",
+	cardBack__answersCover: "w-full h-full flex flex-col justify-center md:p-4",
+	cardBack__btnCover: "w-full flex justify-around   border-t-2 border-blue-400",
 };
 
 const Card = ({ dataTS }: { dataTS: Card_Vocabs[] }) => {
@@ -45,90 +50,92 @@ const Card = ({ dataTS }: { dataTS: Card_Vocabs[] }) => {
   };
 
   return (
-    <motion.section
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
-      className={styles.section}
-    >
-      <motion.div
-        className={styles.section__div}
-        animate={{ rotateY: switchSide ? 0 : 180 }}
-      >
-        <article className={styles.article}>
-          {switchLanguage ? (
-            sentenceTranslated ? (
-              <div>
-                <h3 className={styles.h3}>{sentenceTranslated}</h3>
-                <h3 className={styles.h3}>{wordTranslated}</h3>
-              </div>
-            ) : (
-              <h3 className={styles.h3}>{wordTranslated}</h3>
-            )
-          ) : (
-            <h3 className={styles.h3}>{czWord}</h3>
-          )}
-        </article>
+		<motion.section
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 1 }}
+			className={styles.section}>
+			<motion.div
+				className={styles.section__div}
+				animate={{ rotateY: switchSide ? 0 : 180 }}>
+				<article className={styles.article}>
+					{switchLanguage ? (
+						sentenceTranslated ? (
+							<div>
+								<h3 className={styles.h3}>
+									{sentenceTranslated}</h3>
+								<h3 className={styles.h3}>{wordTranslated}</h3>
 
-        <div className={styles.section__div__btnCover}>
-          <CardButton
-            onClick={() => [setSwitchLanguage(!switchLanguage)]}
-            text={switchLanguage ? "CZ" : "EN"}
-          />
+							</div>
+						) : (
+							<h3 className={styles.h3}>{wordTranslated}</h3>
+						)
+					) : (
+						<h3 className={styles.h3}>{czWord}</h3>
+					)}
+				</article>
 
-          <CardButton onClick={randomWord} text={"Next word"} />
+				<div className={styles.section__div__btnCover}>
+					<button onClick={() => [setSwitchLanguage(!switchLanguage)]}>
+						{switchLanguage ? (
+							<Image src={czFlag} alt="cz-flag" />
+						) : (
+							<Image src={enFlag} alt="eng-flag" />
+						)}
+					</button>
 
-          <CardButton
-            onClick={() => [setSwitchSide(!switchSide)]}
-            text={"Answer"}
-          />
-        </div>
+					<button
+						className={styles.button_red}
+						onClick={() => [setSwitchSide(!switchSide)]}>
+						answer
+					</button>
 
-        <motion.div className={styles.cardBack}>
-          <div className={styles.cardBack__div}>
-            {switchLanguage ? (
-              sentenceTranslated ? (
-                <div className={styles.cardBack__answersCover}>
-                  <h3 className={styles.h3}>{sentenceTranslated}</h3>
-                  <h3 className={styles.h3}>{wordTranslated}</h3>
-                  <h3 className={styles.h3}>{czWord}</h3>
-                </div>
-              ) : (
-                 <div className={styles.cardBack__answersCover}>
-                  <h3 className={styles.h3}>{wordTranslated}</h3>
-                  <h3 className={styles.h3}>{czWord}</h3>
-                </div>
-              )
-            ) : sentenceTranslated ? (
-               <div className={styles.cardBack__answersCover}>
-                <h3 className={styles.h3}>{czWord}</h3>
-                <h3 className={styles.h3}>{sentenceTranslated}</h3>
-                <h3 className={styles.h3}>{wordTranslated}</h3>
-              </div>
-            ) : (
-               <div className={styles.cardBack__answersCover}>
-                <h3 className={styles.h3}>{czWord}</h3>
-                <h3 className={styles.h3}>{wordTranslated}</h3>
-              </div>
-            )}
+					<button className={styles.button_green} onClick={randomWord}>
+						Next word
+					</button>
+				</div>
 
-            <div className={styles.cardBack__btnCover}>
-              <CardButton
-                onClick={() => [setSwitchLanguage(!switchLanguage)]}
-                text={switchLanguage ? "CZ" : "EN"}
-              />
+				<motion.div className={styles.cardBack}>
+					<div className={styles.cardBack__div}>
+						{switchLanguage ? (
+							sentenceTranslated ? (
+								<div className={styles.cardBack__answersCover}>
+									<h3 className={styles.h3}>{sentenceTranslated}</h3>
+									<h3 className={styles.h3}>{wordTranslated}</h3>
+									<h3 className={styles.h3}>{czWord}</h3>
+								</div>
+							) : (
+								<div className={styles.cardBack__answersCover}>
+									<h3 className={styles.h3}>{wordTranslated}</h3>
+									<h3 className={styles.h3}>{czWord}</h3>
+								</div>
+							)
+						) : sentenceTranslated ? (
+							<div className={styles.cardBack__answersCover}>
+								<h3 className={styles.h3}>{czWord}</h3>
+								<h3 className={styles.h3}>{sentenceTranslated}</h3>
+								<h3 className={styles.h3}>{wordTranslated}</h3>
+							</div>
+						) : (
+							<div className={styles.cardBack__answersCover}>
+								<h3 className={styles.h3}>{czWord}</h3>
+								<h3 className={styles.h3}>{wordTranslated}</h3>
+							</div>
+						)}
 
-              <CardButton
-                onClick={() => [setSwitchSide(!switchSide)]}
-                text={"Question"}
-              />
-            </div>
-          </div>
-        </motion.div>
-      </motion.div>
-    </motion.section>
-  );
+						<div className={styles.cardBack__btnCover}>
+							<button
+								className={styles.button_green}
+								onClick={() => [setSwitchSide(!switchSide)]}>
+								Question
+							</button>
+						</div>
+					</div>
+				</motion.div>
+			</motion.div>
+		</motion.section>
+	);
 };
 
 export default Card;
