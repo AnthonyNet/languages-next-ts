82150e7c3b01c829940d3138a22ccd64924cf284
AnthonyNet <zejda.antonin@gmail.com>
2023-06-17 19:44:38 +0200

    style(Navbar): add hover effect for Home btn

--- components/navbar/NavItems.tsx
+++ components/navbar/NavItems.tsx
@@ -11,7 +11,7 @@ const styles = {
 	container:
 		"relative inline-block tooltip hover:text-white transition ease-in-out duration-700",
 	main__a:
-		"transition ease-in-out duration-700 px-2 py-1 font-medium transform hover:-translate-y-6", //ulAnimation
+		"transition ease-in-out duration-500 px-2 py-1 font-medium", //ulAnimation
 	cover:
 		"flex flex-col p-4 bg-white w-60 h-auto rounded-md z-20 absolute right-0 invisible tooltip-item border-2 navCover",
 	ul: "list-disc space-y-2 style-",
@@ -41,32 +41,32 @@ function NavItems() {
 	  })
 	)}
 
-	return <article className={styles.article}>
-					<Link href="/">Home</Link>
+	return (
+		<article className={styles.article}>
+			<Link href="/" className="hover:text-white transition ease-in-out duration-500">
+				Home
+			</Link>
 
-					<div className={styles.container}>
-						<a href="#" className={styles.main__a}>
-							English
-						</a>
-						<div className={styles.cover}>
-							<ul className={styles.ul}>
-								{menuItems(menuData.english)}
-							</ul>
-						</div>
-					</div>
+			<div className={styles.container}>
+				<a href="#" className={styles.main__a}>
+					English
+				</a>
+				<div className={styles.cover}>
+					<ul className={styles.ul}>{menuItems(menuData.english)}</ul>
+				</div>
+			</div>
 
-					<div className={styles.container}>
-						<a href="#" className={styles.main__a}>
-							German
-						</a>
+			<div className={styles.container}>
+				<a href="#" className={styles.main__a}>
+					German
+				</a>
 
-						<div className={styles.cover}>
-							<ul className={styles.ul}>
-								{menuItems(menuData.german)}
-							</ul>
-						</div>
-					</div>
-				</article>
+				<div className={styles.cover}>
+					<ul className={styles.ul}>{menuItems(menuData.german)}</ul>
+				</div>
+			</div>
+		</article>
+	);
 
 
 }
