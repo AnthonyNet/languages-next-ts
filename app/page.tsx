90030aa15d2a36bc6effa16494e46f3602b8d021
AnthonyNet <zejda.antonin@gmail.com>
2023-11-12 15:41:28 +0100

    refactor(indexPage):add tailwind styles back, delete styles constant

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,10 @@ import CloudDown from "../components/icons/CloudDown";
 import Section2 from "../sections/Section2_Index";
 import Flying_objects from "@/components/home/Flying-objects";
 
-const styles = {
-	section:
-		"responsiveSection grid grid-rows-6  items-center justify-center  h-100-dvh",
-	h1__cover:
-		"max-h-[100px] flex items-center justify-center sm:max-h-[500px] row-start-3 row-end-5 cursor-pointer __gradient __text_shadow rounded-tl-3xl rounded-br-3xl -skew-x-12  __css_h1cover_height rounded-tr-xl",
-	h1: "text-[10vw] xl:text-[120px] leading-[160px] text-transparent  bg-clip-text bg-gradient-to-r from-blue-800 to-sky-900 block invert",
-	bottom__container:
-		"row-start-5 row-end-7 justify-center items-center  text-center __css_size",
-	icon__cover:
-		" grid justify-center items-center motion-safe:animate-ping-slower md:mt-4",
-} as const;
-
 function MainPage() {
 	return (
 		<>
-			<section className={styles.section}>
+			<section className="responsiveSection grid grid-rows-6  items-center justify-center  h-100-dvh">
 				<Flying_objects />
 
 				<motion.div
@@ -33,7 +21,7 @@ function MainPage() {
 						duration: 3,
 						ease: "easeOut",
 					}}
-					className={styles.h1__cover}>
+					className="max-h-[100px] flex items-center justify-center sm:max-h-[500px] row-start-3 row-end-5 cursor-pointer __gradient __text_shadow rounded-tl-3xl rounded-br-3xl -skew-x-12  __css_h1cover_height rounded-tr-xl">
 					<div className="relative overflow-hidden ">
 						<h1 className="text-[10vw] xl:text-[120px] leading-[160px] transform  font-bold relative text-transparent  bg-clip-text">
 							<span className="text-blue-700">Learn</span>{" "}
@@ -43,7 +31,7 @@ function MainPage() {
 						</h1>
 					</div>
 				</motion.div>
-				<div className={styles.bottom__container}>
+				<div className="row-start-5 row-end-7 justify-center items-center  text-center __css_size">
 					<h2>Working in progress...</h2>
 					<Typewriter
 						options={{
@@ -59,7 +47,7 @@ function MainPage() {
 							loop: true,
 						}}
 					/>
-					<div className={styles.icon__cover}>
+					<div className="grid justify-center items-center motion-safe:animate-ping-slower md:mt-4">
 						<a href="#section-2">
 							<CloudDown />
 						</a>
